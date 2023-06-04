0c478c2750b5c608c3e15fa051b52bdc77c20999
blueberry077 <118677993+blueberry077@users.noreply.github.com>
2023-06-04 20:24:28 +0200

    hello.c
    The Main File

---
+++ hello.c
@@ -0,0 +1,13 @@
+/* Include Standard Library */
+#include <stdio.h>
+/* Entry Point ............ */
+int main(void)
+	/* The main function is the Entry Point .. *
+	 * of our code. This function prints the . *
+	 * message: "Hello Github" to the terminal *
+	 * and returns the 0 to the OS. .......... */
+{
+	(void)printf("Hello Github !\n");
+	return 0;
+}
+/* ........................ */
